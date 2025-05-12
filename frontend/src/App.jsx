a0f057ac15a56763d4e839dab8b2d18e2c496de5
Huỳnh Vũ Khang <146689046+khang270607@users.noreply.github.com>
2025-05-12 21:35:41 +0700

    fix get product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ import UserManagement from '~/pages/admin/UserManagement/index'
 import ProductManagement from '~/pages/admin/ProductManagement/index.jsx'
 import CategorieManagement from '~/pages/admin/CategorieManagement/index.jsx'
 import OrderManagement from '~/pages/admin/OrderManagement/index'
-import Profile from '~/pages/admin/Profile/index.jsx'
+import ProfileAdmin from '~/pages/admin/Profile/index.jsx'
 
 // Trang 404
 import NotFound from '~/pages/404/NotFound'
@@ -72,7 +72,7 @@ function App() {
             element={<CategorieManagement />}
           />
           <Route path='order-management' element={<OrderManagement />} />
-          <Route path='profile' element={<Profile />} />
+          <Route path='profile' element={<ProfileAdmin />} />
         </Route>
       </Route>
 

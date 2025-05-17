a28a8f5044670329766ef2ba7f25cbff84724921
Nhuttruongle <146928083+Nhuttruongle@users.noreply.github.com>
2025-05-17 11:34:18 +0700

    savecodeee

--- frontend/src/pages/user/Home/UserHome.jsx
+++ frontend/src/pages/user/Home/UserHome.jsx
@@ -4,12 +4,13 @@ import Slider from './Slider/Slider'
 import ProductCategories from './ProductCategories/ProductCategories'
 import ProductList from './ProductList/ProductList'
 import StoreIntroduction from './StoreIntroduction/StoreIntroduction'
-
+import CouponList from './CouponList/CouponList'
 const UserHome = () => {
   return (
     <>
       <Slider />
       <ProductCategories />
+      <CouponList />
       <ProductList />
       <StoreIntroduction />
     </>
